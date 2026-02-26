78bee50a8c58f27e5d2c8f36ca1be879ae02569d
Akhilesh <prathibhasolutions1@gmail.com>
2026-02-26 20:30:49 +0530

    Fixed gunicorn bind for EB

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mocktest_platform.wsgi --log-file -
+web: gunicorn mocktest_platform.wsgi:application --bind 0.0.0.0:8000
--- mocktest_platform/settings.py
+++ mocktest_platform/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'mocktest_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'sherLOCKED123$%^',
+        'HOST': 'exampattern-db.c922kmuauwy1.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
