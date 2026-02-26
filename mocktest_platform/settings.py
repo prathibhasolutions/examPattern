90cef6558f422682407336664b7ed5e9a26721e7
Akhilesh <prathibhasolutions1@gmail.com>
2026-02-26 20:35:49 +0530

    Added collectstatic for EB

--- mocktest_platform/settings.py
+++ mocktest_platform/settings.py
@@ -131,9 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (user uploads)
 MEDIA_URL = '/media/'
