65e41493f0dcaaf2f5f70fbae98f45f44bb91c12
Akhilesh <prathibhasolutions1@gmail.com>
2026-02-26 20:42:28 +0530

    Configured nginx to serve static and media

--- mocktest_platform/settings.py
+++ mocktest_platform/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
